2de3bfcad4fc61edd16fe81301f83aa89d64fc79
rk <>
2021-05-31 11:35:02 +0900

    api で求人情報取得に成功

--- js/ajax.js
+++ js/ajax.js
@@ -1,4 +1,27 @@
 $(function() {
+
+  
+  Object.keys(prefectures).forEach((key) => {
+    let list = `<li>${prefectures[key]}</li>`
+    
+    $("#search ul").append(list);
+  });
+  
+  // $.ajax({
+  //     type: "GET",
+  //     url: `https://www.land.mlit.go.jp/webland/api/TradeListSearch?from=20151&to=20152&area=${}`,
+  //     dataType : "json"
+  //   })
+  //   // Ajaxリクエストが成功した場合
+  //   .done(function(data){
+  //     console.log(data.results[0]);
+  //   })
+  //   // Ajaxリクエストが失敗した場合
+  //   .fail(function(XMLHttpRequest, textStatus, errorThrown){
+  //     alert(errorThrown);
+  //   });
+
+  
     
     $.ajax({
         type: "GET",
@@ -7,7 +30,7 @@ $(function() {
       })
       // Ajaxリクエストが成功した場合
       .done(function(data){
-        console.log(data.results[0].title);
+        console.log(data.results[0]);
       })
       // Ajaxリクエストが失敗した場合
       .fail(function(XMLHttpRequest, textStatus, errorThrown){
